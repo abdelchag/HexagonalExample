8ec1ae577bb0a6c604674f9700527e14b8c253d0
ABDELCHAG <abdellah.chagdani@gmail.com>
2019-02-23 15:07:18 +0100

    init project

---
+++ src/main/java/org/abdel/domain/Produit.java
@@ -0,0 +1,47 @@
+package org.abdel.application.domain;
+
+/**
+ * Created by ABDELCHAG on 23/02/2019.
+ */
+public class Produit {
+
+    private Long id;
+
+    private String designation;
+
+    private Integer prix;
+
+    private Integer quantite;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public Integer getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Integer prix) {
+        this.prix = prix;
+    }
+
+    public Integer getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(Integer quantite) {
+        this.quantite = quantite;
+    }
+}
